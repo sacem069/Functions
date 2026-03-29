13da407bedfcb7449d2c3bc0c26bf14a81086484
sacem069 <sacem069@newschool.edu>
2026-03-29 17:51:46 -0400

    filtering time tools and days

--- assets/main.js
+++ assets/main.js
@@ -14,10 +14,22 @@ formElement.addEventListener('submit', (event) => {
     let selectedDay= document.getElementById('day-select').value
     let selectedTime =	document.getElementById('time-select').value
 	console.log(`Tool: ${selectedTool}, Day: ${selectedDay}, Time: ${selectedTime}`)
-	let results = allData.filter (item => item.tools.includes(selectedTool))
-	console.log(results)
 
+	let results = 
+	allData.filter ((item) => {
+		if (selectedDay == "Saturday" || selectedDay === "Sunday") {
+			return item.tools.includes(selectedTool) && 
+	item.weekend_days.includes(selectedDay)
+		} else {
+			return item.tools.includes(selectedTool) &&
+			selectedTime >= item.weekday_open &&
+			selectedTime <= item.weekday_close
+
+		}	
 })
+  console.log(results)
+})
+
 
 fetch('assets/data.json')
 	.then(response => response.json())
