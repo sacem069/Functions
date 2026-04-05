f84f0fdf54c04d016723175400e891e5b99b19d5
sacem069 <sacem069@newschool.edu>
2026-04-05 10:58:03 -0400

    fixed results filter

--- assets/main.js
+++ assets/main.js
@@ -31,11 +31,12 @@ let formElement = document.getElementById('making-center-form')
 formElement.addEventListener('submit', (event) => {
 	event.preventDefault()
 	console.log('submitted!')
-	let selectedTool3D = document.getElementById('tool-select-3D').value
+	let selectedTool3D = document.getElementById('tool-select-3D' ||  'tool-select-print' || 'tool-select-media' || 'tool-select-wood' || 'tool-select-sewing').value
 	let selectedToolPrint = document.getElementById('tool-select-print').value
 	let selectedToolMedia = document.getElementById('tool-select-media').value
 	let selectedToolWood = document.getElementById('tool-select-wood').value
 	let selectedToolSewing = document.getElementById('tool-select-sewing').value
+	let selectedTool = selectedTool3D || selectedToolPrint || selectedToolMedia || selectedToolWood || selectedToolSewing
 	let selectedDay = document.getElementById('day-select').value
 	let selectedTime = document.getElementById('time-select').value
 	console.log(`Tool: ${selectedTool}, Day: ${selectedDay}, Time: ${selectedTime}`)
@@ -43,10 +44,10 @@ formElement.addEventListener('submit', (event) => {
 	let results =
 		allData.filter((item) => {
 			if (selectedDay == "Saturday" || selectedDay === "Sunday") {
-				return item.tools.includes(selectedTool3D && selectedToolPrint && selectedToolMedia && selectedToolWood && selectedToolSewing) &&
+				return item.tools.includes(selectedTool) &&
 					item.weekend_days.includes(selectedDay)
 			} else {
-				return item.tools.includes(selectedTool3D && selectedToolPrint && selectedToolMedia && selectedToolWood && selectedToolSewing) &&
+				return item.tools.includes(selectedTool) &&
 					selectedTime >= item.weekday_open &&
 					selectedTime <= item.weekday_close
 			}
