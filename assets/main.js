ebe9cac4595b2da80a9b42eb23654241e420b6a5
sacem069 <sacem069@newschool.edu>
2026-04-12 14:14:12 -0400

    break down labs from facilities

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,8 @@ const facilityColors = {
 	"Open Work Spaces": "#F14A02",
 }
 
+const facilities = ['3D & Digital Prototyping', 'Print', 'Media', 'Wood, Metal and Ceramics', 'Sewing and Textiles', 'Open Work Spaces']
+
 
 let renderItems = (data) => {
 	let dataList = document.getElementById('makingcenter-list')
@@ -41,6 +43,60 @@ let renderItems = (data) => {
 	})
 }
 
+let renderFacilities = (facilities) => {
+	let toolPicker = document.getElementById('tool-picker')
+	toolPicker.innerHTML = ''
+
+	facilities.forEach((facility) => {
+		let listItem = `
+			<button type="button" style="background-color: ${facilityColors[facility]}; color: white;" value="${facility}" class="facility-btn">${facility} </button>`
+
+
+		toolPicker.insertAdjacentHTML('beforeend', listItem)
+
+	})
+	document.querySelectorAll('.facility-btn').forEach((btn) => {
+		btn.addEventListener('click', (event) => {
+			let LabsInFacilities = allData.filter((item) => {
+				return item.facility === event.target.value
+			})
+			document.getElementById('tool-select').value = event.target.value;
+			document.getElementById('tool-dropdown').innerHTML = ''
+			renderLabs(LabsInFacilities);
+
+		})
+	})
+}
+
+let renderLabs = (labs) => {
+	let labPicker = document.getElementById('lab-picker')
+	labPicker.innerHTML = ''
+
+	labs.forEach((lab) => {
+		let listItem = `
+			<button type="button" style="background-color: ${facilityColors[lab.facility]}; color: white;" value="${lab.name}" class="lab-btn">${lab.name} </button>
+		`
+		labPicker.insertAdjacentHTML('beforeend', listItem)
+	})
+	document.querySelectorAll('.lab-btn').forEach((btn) => {
+		btn.addEventListener('click', (event) => {
+			let selectedLab = allData.find((item) => {
+				return item.name === event.target.value
+			})
+			let toolsArray = selectedLab.tools.split(', ')
+			let toolDropdown = document.getElementById('tool-dropdown')
+			toolDropdown.innerHTML = `<select id="tool-select-dropdown">
+    ${toolsArray.map(tool => `<option value="${tool}">${tool}</option>`).join('')}</select>`
+			document.getElementById('tool-select-dropdown').addEventListener('change', (e) => {
+				document.getElementById('tool-select').value = e.target.value
+
+			})
+
+
+		})
+	})
+}
+
 let allData = []
 
 let formElement = document.getElementById('making-center-form')
@@ -48,7 +104,7 @@ let formElement = document.getElementById('making-center-form')
 //Exact time by default
 // used this example as reference to set the time input to the current time: https://codepen.io/mfehrenbach/pen/jEMpamr?editors=1010
 //selecting the time input 
-const timeInput = document.getElementById('time-select') 
+const timeInput = document.getElementById('time-select')
 //getting current day and time
 const rightNow = new Date()
 //as get hours returns a number we need a string to use the padStart method to add a leading zero if the hour is less than 10
@@ -59,7 +115,7 @@ timeInput.value = `${hours}:${minutes}`
 
 
 //Exact day by default
-const dayInput = document.getElementById('day-select') 
+const dayInput = document.getElementById('day-select')
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 dayInput.value = days[rightNow.getDay()]
 
@@ -67,13 +123,7 @@ dayInput.value = days[rightNow.getDay()]
 formElement.addEventListener('submit', (event) => {
 	event.preventDefault()
 	console.log('submitted!')
-	let selectedTool3D = document.getElementById('tool-select-3D').value
-	let selectedToolPrint = document.getElementById('tool-select-print').value
-	let selectedToolMedia = document.getElementById('tool-select-media').value
-	let selectedToolWood = document.getElementById('tool-select-wood').value
-	let selectedToolSewing = document.getElementById('tool-select-sewing').value
-	let selectedToolOpen = document.getElementById('tool-select-open').value
-	let selectedTool = selectedTool3D || selectedToolPrint || selectedToolMedia || selectedToolWood || selectedToolSewing || selectedToolOpen
+	let selectedTool = document.getElementById('tool-select').value
 	let selectedDay = document.getElementById('day-select').value
 	let selectedTime = document.getElementById('time-select').value
 	console.log(`Tool: ${selectedTool}, Day: ${selectedDay}, Time: ${selectedTime}`)
@@ -97,12 +147,11 @@ formElement.addEventListener('submit', (event) => {
 
 
 
-
-
 fetch('assets/data.json')
 	.then(response => response.json())
 	.then(data => {
 		allData = data
+		renderFacilities(facilities)
 		// And passes the data to the function, above!
 	})
 
